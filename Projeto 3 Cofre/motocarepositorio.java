77b7647719a5a1c8ace1e47afa508cfc9aa680b3
DiorIceCream <74986490+DiorIceCream@users.noreply.github.com>
2020-12-13 23:50:29 -0600

    Add files via upload
    repositorio projeto 3

---
+++ Projeto 3 Cofre/motocarepositorio.java
@@ -0,0 +1,19 @@
+/*
+
+Oi Prof eu fiz o projeto da motoca ja que tinhamos visto bem o tema na aula.
+Comecei fazendo pensando que ia ser igual a do carro mas ficou um pouco mas complicado que isso.
+Aprendi utilizar duas classes e como chamar elas.
+
+Fiquei bem enrolado em na segunda parte onde precisava fazer uma pessoa Subir e tambem prencheer seus dados, ou seja o nome e idade.
+Ai foi onde eu buguei ainda assinstindo a aulas fiquei perdido pq eu tava fazendo a motoca interativa mas nas aulas so era a main nao interativa.
+Mas tudo bem resolvi falando com os monitores e o Juan David me ensino direitnho como eu precisava fazer.
+Entendi entao o meu erro e assim acertei e segui por frente dai era muito similar ao carro que eu ja tinha feito so com poucas modificacoes.
+
+Alem disso vi que o monitor me perguntou se eu sabia utilizar arrays. 
+Ai di uma olhado no arraylist que vc botou mas nao consegui entender bem direitinho como utilizar isso.
+Mas entendi que e uma forma de armazenar multiples coisas como por exemplo strings. 
+Enfim foi bem estressado para mim resolver sozinho sem ajuda do monitor.
+Eu dediquei 1 hora um dia assintindo a aula, o seguinte dia dediquei 5 horas tentando fazer mas nao resolvi e o ultimo dia dediquei 2 horas completando a atividade.
+
+
+*/
