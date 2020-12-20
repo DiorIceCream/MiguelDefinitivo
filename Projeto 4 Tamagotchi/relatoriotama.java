bfd22d54fcfe9813c2651e9d1876114450001b45
DiorIceCream <74986490+DiorIceCream@users.noreply.github.com>
2020-12-20 15:23:36 -0600

    Add files via upload

---
+++ Projeto 4 Tamagotchi/relatoriotama.java
@@ -0,0 +1,16 @@
+/* 
+Oi Prof
+
+Meu relatorio do tamagotchi
+
+Eu fiz sozinho mas fui me baseando no exemplo que fizemos na aula 
+entao eu acho que ficou bem similar e sei la se isso e plagio mas tentei aprender pq nao tava entendendo o do private e get e set mas eu acho que ficou bem claro para mim
+entao comecei fazendo como sempre eu fiz os parametros e o construtor
+fiz o toString tambem 
+comecei olhando o set e get
+e entendi como e utilizado e literalmente o que fala set eh para determinar um valor
+e get e para returna ele
+
+Mas agora vou tentar fazer o projeto extra para tentar fazer algo do 0
+
+*/
