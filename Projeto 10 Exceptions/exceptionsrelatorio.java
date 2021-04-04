d643dba8e6a76f8c067bc70ff95bad43a158936f
DiorIceCream <74986490+DiorIceCream@users.noreply.github.com>
2021-04-04 15:35:21 -0600

    Aqui o meu codigo prof

---
+++ Projeto 10 Exceptions/exceptionsrelatorio.java
@@ -0,0 +1,30 @@
+/* 
+
+Oi David,
+
+espero tudo esteja bem com voce 
+aqui o meu codigo das exceptions 
+na real nao entendi muito bem a aula e tinha te falado disso mas denovo fiz uma pequena pesquisa na net e eu acho q consegui fazer o q vc pediu
+eu fiz sozinho e fiz por cima do codigo de favoritos da agenda
+
+assisti a sua aula mas nao entendi muito bem ai eu comecei ler um pouco sobre exceptions in java
+ai vi q eh uma forma de amostrar erros no codigo sem fazer os sysouts q nos fazemos
+
+aprendi das exceptions e como as exceptions eh toda uma familia e tem muitos exceptions como por exemplo o index out of bounds exceptions
+q eh um exception por exemplo no meu codigo quando o index nao consegui procurar nos index ja criados e procura afora disso
+aqui aprendi a fazer o try e catch q literalmente eh isso o codigo "tenta" fazer.
+
+tambem comprendi q o codigo nao se fecha quando o erro acontece ja que so mostra o exception com a sua mensagem
+tambem aprendi a trocar essas mensagems por exemplo a da search q fiz e troquei ela por User or Number not found q eh quando o codigo nao encontre o usario ou numero
+
+gostei muito desta atividade mais q tudo foi de pesquisar bem e ler um pouco sobre estas exceptions
+
+eu fiz exceptions na main interativa como por exemplo quando o usario faz um comando q nao exista 
+tambem fiz outro no search por se acaso o usuario ou numero nao existir 
+tambem fiz outro no add q era por se acaso o usario so digitar um dos dois parametros q precisa o codigo
+fiz no remove por se acaso nao encontrar o usuario para remover
+
+
+
+
+*/
