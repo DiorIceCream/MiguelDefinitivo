05b95d6cf73cd6f19bc72f4abd28ca245eed6124
DiorIceCream <74986490+DiorIceCream@users.noreply.github.com>
2021-04-04 15:20:34 -0600

    Outro Codigo

---
+++ Projeto 9 Favoritos/Agenda.java
@@ -0,0 +1,263 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Contact {
+    String name;
+    ArrayList <Fone> fones = new ArrayList<Fone>();
+    boolean starred;
+
+    Contact(String name){
+        this.name = name;
+        this.starred = false;
+    }
+
+    void addFone(String label, String number){
+        this.fones.add(new Fone(label, number));
+    }
+    
+    void rmFone(int index){
+        if(index < 0 && index > this.fones.size()){
+            System.out.println("Invalid");
+        }else {
+            fones.remove(index);
+        }
+    }
+
+    void addStarred(){
+        starred = true;
+    }
+
+    void rmStarred(){
+        starred = false;
+    }
+
+    public String toString(){
+        String s = starred ? " @ " : " - ";
+        return s + name + fones.toString();
+    } 
+
+}
+
+class Fone{
+    String label;
+    String number;
+
+    //Fone(serial){
+
+    // }
+
+    Fone(String id, String number) {
+        System.out.println(id);
+        System.out.println(number);
+        this.label = id;
+        this. number = number;
+    }
+
+    boolean validate(String number){
+        return true;
+    }
+
+    public String toString(){
+        return this.number;
+    }
+}
+
+class Lista{
+    ArrayList <Contact> contacts = new ArrayList<Contact>();
+
+    Lista(){
+
+    }
+
+    int findContact(String name){
+        int index = -1;
+        for(int i = 0; i < contacts.size(); i++){
+            if(contacts.get(i).name.equals(name)){
+                index = i; 
+            }
+        }
+        return index;
+    }
+
+    void addContact(String name, Fone fone){
+
+        if(contacts.size() == 0){
+            Contact c = new Contact(name);
+                c.addFone(fone.label, fone.number);
+                contacts.add(c);
+        } else {
+            for (Iterator<Contact> it = contacts.iterator(); it.hasNext(); ) {
+                Contact d = it.next();
+                if(d.name.equals(name)){
+                    d.fones.add(fone);
+                    break;
+                }else{ 
+                    Contact c = new Contact(name);
+                    c.addFone(fone.label, fone.number);
+                    contacts.add(c);
+                    break;
+                }
+            }
+            
+        }
+        
+    }
+
+    boolean rmContact(String name){
+        int index = -1;
+        for(int i = 0; i < contacts.size(); i++){
+            if(contacts.get(i).name.equals(name)){
+                index = i; 
+                System.out.println("found index" + i);
+            }
+        } 
+
+        if(index != -1){
+            if(contacts.remove(index) != null){
+                return true;
+            }
+             
+        } 
+        return false;    
+    }
+    
+    /*
+    for(int i = 0; i < contacts.size(); i++){
+        if(contacts.get(i).name.equals(name)){
+            return i;
+        } else {
+            return -1;
+        }
+    }
+    */
+
+    ArrayList<Contact> search(String pattern){
+        ArrayList <Integer> indexes = new ArrayList <Integer>();
+        ArrayList <Contact> results = new ArrayList <Contact>();
+
+        for(int i = 0; i < contacts.size(); i++){
+            if(contacts.get(i).name.contains(pattern)){
+                indexes.add(i);
+            } 
+        }
+        if(indexes.size() > 0){
+            for(int i = 0; i < indexes.size(); i++){
+                results.add(contacts.get(i));
+        
+            }
+        }
+        return results.size() > 0 ? results : null;
+    }   
+
+    ArrayList<Contact> searchphone(String pattern){
+
+        ArrayList <Integer> indexes = new ArrayList <Integer>();
+        ArrayList <Contact> results = new ArrayList <Contact>();
+
+        for(int i = 0; i < contacts.size(); i++){
+            for(int j = 0; j < contacts.get(i).fones.size(); j++){
+                if(contacts.get(i).fones.get(j).number.contains(pattern)){
+                    indexes.add(i);
+                }
+            } 
+        }
+        if(indexes.size() > 0){
+            for(int i = 0; i < indexes.size(); i++){
+                results.add(contacts.get(i));
+        
+            }
+        }
+        return results.size() > 0 ? results : null;
+        
+    }
+
+    ArrayList<Contact> getContacts(){
+        return contacts;
+    }
+
+    Contact getContact(String name){
+        int index = findContact(name);
+        if(index != -1){
+            return contacts.get(findContact(name));
+        } 
+        return null;
+    }
+
+    void bookmark(String id){
+        Contact contact = getContact(id);
+        contact.addStarred();
+    }
+
+    void unbookmark(String id){
+        Contact contact = getContact(id);
+        contact.rmStarred();
+    }
+
+    ArrayList<Contact> getBookmarks(){
+        ArrayList <Contact> bookmarks = new ArrayList<Contact>();
+        for(int j = 0; j < contacts.size(); j++){
+            if(contacts.get(j).starred){
+                bookmarks.add(contacts.get(j));
+            }
+        } 
+        return bookmarks;
+    }
+
+    public String toString(){
+        String x = "";
+        if(contacts.size() > 0){
+            for (int counter = 0; counter < contacts.size(); counter++) { 		      
+                x += contacts.get(counter).name + "-" + contacts.get(counter).fones ; 		
+            }  
+        }
+         return x;
+    }
+
+    
+
+}
+
+
+
+    
+public class Agenda{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Lista list = new Lista(); 
+        while(true){
+            String line = scanner.nextLine();
+            String[] ui = line.split(" ");
+            if(ui[0].equals("stop")){
+                break;
+            } else if(ui[0].equals("show")){
+                System.out.println(list.toString());
+            } else if(ui[0].equals("remove")){
+                list.rmContact((ui[1]));
+            } else if(ui[0].equals("add")){
+                Fone f = new Fone(ui[1], ui[2]);
+                list.addContact((ui[1]), f);
+            } else if(ui[0].equals("search")){
+                ArrayList <Contact> results = new ArrayList<Contact>();
+                results = list.search(ui[1]);
+
+
+                ArrayList <Contact> phoneresults = new ArrayList <Contact>();
+                phoneresults = list.searchphone(ui[1]);
+                
+                    System.out.println(results);
+                    System.out.println(phoneresults);
+               
+                
+            } else if(ui[0].equals("starred")){
+                System.out.println(list.getBookmarks());
+            } else if(ui[0].equals("star")){
+                list.bookmark(ui[1]);
+            } else if(ui[0].equals("unstar")){
+                list.unbookmark(ui[1]);
+            }
+                
+        } 
+        scanner.close();
+    }
+}
---
+++ Projeto 9 Favoritos/favoritosrelatorio.java
@@ -0,0 +1,29 @@
+/* 
+
+Oi Prof,
+
+espero tudo esteja bem com vc aqui o meu codigo dos favoritos
+como tinha falado antes realmente me esforcei muito no ultimo codigo ja que dai eu ia conseguir resolver os outros projetos
+eu fiz sozinho como sempre e com ajuda do meu codigo passado da agenda
+
+aqui comecei como sempre com o codigo da agenda e comecei seguindo o diagrama.
+como sempre eh uma ajuda grande seguir esse diagrama
+
+comecei acrescentando na classe contact um boolean dos favoritados
+e acrescentei dois metodos de tipo void
+um para add os favoritos e outro para remover os favoritos
+logo fiz um toString diferente para esses favoritos para cada um ter um @ ou - e aprovetei utilizar um inline if ai
+
+na classe Lista q eh a agenda eu comecei acresentando os novos metodos
+o void bookmark e o void unbookmark
+ai eu fiz um getbookmark de tipo array contact e ai eh onde se acresentam os favoritos 
+na real a logica foi simples pq resolvi ela utilizando esse boolean e verificando se ou nao podia se acresentar ou remover os favoritos
+
+neste caso nao fiquei muito estressado ja que era de acresentar por cima dum codigo e gracas a deus resolvi numas quantas horas
+
+tambem acrescentei na main interativa os comandos para os favoritos 
+
+
+
+
+*/
